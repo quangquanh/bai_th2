b48ba9599d2937f786d0ba61dddbb3acc3e77201
wang <doquang227@email.com>
2022-05-13 15:16:29 +0700

    c2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 var app = express();
 const port = 3000;
 app.get("/", (req, res) =>
-  res.json({ messenger: "API is running in port 3000 C1" })
+  res.json({ messenger: "API is running in port 3000" })
 );
 app.get("/hello")
 app.listen(port, () => console.log("app running in http://localhost:3000"));
